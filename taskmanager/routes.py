d46899c09e473c7948632a0fb353250ab990db8e
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-09 12:40:04 +0000

    imported flask modules. Added post method to the category app.route

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template
+"""
+importing required libraries and modules
+"""
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
+
 
 @app.route("/")
 def home():
@@ -21,8 +25,15 @@ def categories():
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     """
-    When the user clicks to add a new category it should render the template
-    that contains a form, by displaying the form to users we are using the
-    GET method to get the page. When the user submits the form it uses POST method
+    When the user clicks add category button - form template is rendered.
+    When displaying the form we're using the GET method to get the page.
+    When the user submits the form it uses POST method
     """
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+# Look into defensive programming and error handling
