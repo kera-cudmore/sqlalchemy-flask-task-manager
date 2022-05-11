e30a6253553c11aff823042a3f6c857601cd9bac
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-11 16:18:20 +0000

    updated the edit_category route to allow category to be updated and user returned to category page after

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -45,4 +45,9 @@ def edit_category(category_id):
     """
     edit category function
     """
-    return render_template("edit_category.html")
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
