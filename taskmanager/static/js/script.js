d08f5d1e3a1bae9814726b9568ce6902a88cf289
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-12 11:40:00 +0000

    added JS for the datepicker - formatted date & changed button from ok to select

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialization
+
+    // Sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
   });
 
