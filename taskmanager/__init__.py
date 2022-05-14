77fc44f1b240f9d6fe96cfbcdef6ac0d42cc5d0d
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-14 13:42:49 +0000

    update the __init__.py file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,26 +2,36 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    """
-    Imports the hidden environment variables from our 'env' package.
-    As we aren't pushing the env.py file to GitHub it won't be visible
-    once we have deployed to Heroku & will throw an error.
-    So we will only import 'env' if the OS can find an existing file path
-    for the env.py file itself.
-    """
     import env
+
+
+# ^ Imports the hidden environment variables from our 'env' package
+# As we aren't pushing the env.py file to GitHub it won't be visible
+# once we have deployed to Heroku & will throw an error.
+# So we will only import 'env' if the OS can find an existing file path
+# for the env.py file itself.
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+
+# ^ if development == True it will use the local db
+# if false it will use the db on heroku
+
 
 db = SQLAlchemy(app)
 
 from taskmanager import routes
 
 
-# Custom import of routes from taskmanager will throw an error saying it
+# ^ Custom import of routes from taskmanager will throw an error saying it
 # should be installed at the top of the file
 
 # As the routes file relies on both the app and db variables they must be
