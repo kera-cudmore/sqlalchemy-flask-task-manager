89abb51d0500f6981f22ec18a82a452bcfa4da1c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-05-14 11:36:56 +0000

    added in edit task functionality and button

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -53,7 +53,7 @@ def edit_category(category_id):
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
- 
+
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     """
@@ -71,7 +71,7 @@ def delete_category(category_id):
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
     """
-    When the user clicks add category button - form template is rendered.
+    When the user clicks add task button - form template is rendered.
     When displaying the form we're using the GET method to get the page.
     When the user submits the form it uses POST method
     """
@@ -88,3 +88,22 @@ def add_task():
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    """
+    When the user clicks edit task button - form template is rendered.
+    When displaying the form we're using the GET method to get the page.
+    When the user submits the form it uses POST method
+    """
+    task = Task.query.get_or_404(task_id)
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task.task_name=request.form.get("task_name"),
+        task.task_description=request.form.get("task_description"),
+        task.is_urgent=bool(True if request.form.get("is_urgent") else False),
+        task.due_date=request.form.get("due_date"),
+        task.category_id=request.form.get("category_id")
+        db.session.commit()
+    return render_template("edit_task.html", task=task, categories=categories)
